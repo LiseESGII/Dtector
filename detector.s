2d0087022cd315b6b0b13b5f0577fe5098c7a743
LiseESGII <lisebpub@outlook.fr>
2025-05-27 14:11:01 +0200

    [ADD] : afficher le banner

--- detector.s
+++ detector.s
@@ -9,3 +9,20 @@
     safe_msg:       .ascii "[OK] Code semble sain\n\0"
     danger_msg:     .ascii "[ALERTE] Menace detectee: \0"
     newline:        .ascii "\n\0"
+
+.section .text
+.global _start
+
+_start:
+    # Affichage et lecture
+    call show_banner
+    call get_input
+
+# Afficher le banner
+show_banner:
+    mov $1, %rax        # write syscall
+    mov $1, %rdi        # stdout
+    mov $banner, %rsi   # message
+    mov $31, %rdx       # taille
+    syscall
+    ret
